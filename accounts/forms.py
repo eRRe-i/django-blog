de41b4a17a940ed81332f80a7df48d3d55055287
Leandro Bataglia <leandro.bataglia@gmail.com>
2022-04-17 16:30:29 -0300

    Auto stash before merge of "main" and "origin/main"
    started app accounts && added signup and mail validation

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# precisa verificar a unicidade do username
+class UserCreationFormWithMail(UserCreationForm):
+
+    email = forms.EmailField(required=True, help_text="Campo obrigatório")
+
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise(forms.ValidationError("E-mail já cadastrado."))
+        else:
+            return email
+
