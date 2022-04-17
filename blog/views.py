d2f39f14874c29c48d60e384636109cf61f062a2
Leandro Bataglia <leandro.bataglia@gmail.com>
2022-04-17 07:39:37 -0300

    messages inserted

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from . models import Post
 
 # Create your views here.
@@ -15,24 +17,43 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-
-class BlogCreateView(CreateView):
+# AINDA PRECISA TIRAR DO USUÁRIO A TAREFA DE CRIAR O SLUG
+class BlogCreateView(SuccessMessageMixin, CreateView):
     model = Post
     template_name = 'blog/post_new.html'
     fields = ('title', 'slug', 'content', 'author')
+    success_message = "%(field)s criado com sucesso."
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.title,
+        )
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(SuccessMessageMixin, UpdateView):
     model = Post
     template_name = 'blog/post_update.html'
     fields = ('title', 'content')
+    success_message = "%(field)s alterado com sucesso."
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.title,
+        )
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(SuccessMessageMixin, DeleteView):
     model = Post
     template_name = 'blog/post_delete.html'
     success_url = reverse_lazy('home')
 
+    success_message = "Postagem deletada com sucesso."
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BlogDeleteView, self).delete(request, *args, **kwargs)
 
 #     title = models.CharField(max_length=250)
 #     slug = models.SlugField(max_length=250)
